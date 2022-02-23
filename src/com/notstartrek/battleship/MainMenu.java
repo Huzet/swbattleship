f10b55b9b65226048fc289f68afb1151c5f2b69b
Bryan Guerrero <guerrerob318@gmail.com>
2022-02-23 14:11:58 -0800

    added banner and main menu tests

--- src/com/notstartrek/battleship/MainMenu.java
+++ src/com/notstartrek/battleship/MainMenu.java
@@ -4,12 +4,13 @@ package com.notstartrek.battleship;
  * Player Main Menu
  */
 
+import com.apps.util.Console;
 import com.apps.util.Prompter;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+
 
 class MainMenu {
     private Controller player;
@@ -20,9 +21,10 @@ class MainMenu {
     private String name;
 
     // CONSTRUCTOR
-    MainMenu(Prompter prompter){
+    public MainMenu(Prompter prompter){
         this.prompter = prompter;
     }
+
     public void execute() throws IOException {
         boolean runGame = true;
             scoreBoard = new ArrayList<>();
@@ -37,10 +39,12 @@ class MainMenu {
     }
 
     private void welcome() throws IOException {
-        String banner = Files.readString(Path.of("resources/banner.txt"));
+        String banner = Files.readString(Path.of("resources/banner2.txt"));
         System.out.println(banner);
     }
+
     private void createPlayer() {
+        System.out.println("\n");
         String name = prompter.prompt("Please enter player's name: ");
         System.out.println("Welcome to Star Wars Battleship " +  name);
         this.name = name;
@@ -49,11 +53,14 @@ class MainMenu {
     private void promptToPlay() {
         boolean validInput = false;
         while (!validInput) {
+            System.out.println("\n");
             String play = prompter.prompt("Please enter [S]tart Game to continue or [Q]uit to exit the game: ").toUpperCase(Locale.ROOT);
             if (play.matches("S|Q")) { //any digits, one or two times - now proceed
                 validInput = true;
                 if ("S".equals(play)) {
+                    Console.clear();
                     continue;
+
                 } else {
                     System.exit(0);
                 }
@@ -68,12 +75,12 @@ class MainMenu {
 
             scoreBoard.add(player.getTurnCount());
 
-            System.out.println("Star Wars Score Board");
-            System.out.println("______________________________");
+            System.out.println("Star Wars Battleship Score Board");
+            System.out.println("________________________________");
             System.out.println("Name       |     Score");
             for (int i=0; i<scoreBoard.size(); i++){
                 System.out.println(name + "      |     " + scoreBoard.get(i));
-                System.out.println("______________________________");
+                System.out.println("________________________________");
             }
 
             setPreviousScore(player.getTurnCount());
@@ -82,6 +89,7 @@ class MainMenu {
             if (play.matches("P|Q")) { //any digits, one or two times - now proceed
                 validInput = true;
                 if ("P".equals(play)) {
+                    Console.clear();
                     continue;
                 } else {
                     System.exit(0);
