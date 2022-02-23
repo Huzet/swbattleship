32f0b4d8118b7a4781ffaf59f494e4bf4c77fca2
Neten Shrestha <netenshrestha@gmail.com>
2022-02-23 14:53:46 -0500

    added score board

--- src/com/notstartrek/battleship/Main.java
+++ src/com/notstartrek/battleship/Main.java
@@ -10,19 +10,6 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args){
-//
-//        Controller game = new Controller(1,5);
-//        game.gameRunnerSinglePlayer();
-
-
-//        BoardSizes boardSizes = new BoardSizes();
-//        ArrayList<Ship> enemyShips = boardSizes.generateBoardShips();
-////        System.out.println(enemyShips.size());
-//        for (int x = 0; x <= enemyShips.size() - 1; x++) {
-//            boolean roomForShip = true;
-//            int lengthOfShip = enemyShips.get(x).getSize();
-//            System.out.println(lengthOfShip);
-//            System.out.println( enemyShips.get(x).getShipType());
 
         MainMenu menu = new MainMenu(new Prompter(new Scanner(System.in)));
         try {
---
+++ test/com/notstartrek/battleship/MainMenuTest.java
@@ -0,0 +1,5 @@
+package com.notstartrek.battleship;
+
+class MainMenuTest {
+
+}
